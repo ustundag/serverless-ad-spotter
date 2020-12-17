4b97715c4b8445ab0ed13fa5cc344a96a90f240e
ustundag <ustundaganil@gmail.com>
2020-12-17 23:13:01 +0300

    validate beta.json file.

---
+++ upload_pipeline_files.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+function usage {
+     echo """
+     This script is to update the cloudformation files. To use, pass the S3 bucket name.
+
+     ex:
+     upload_pipeline_files.sh s3-bucket-name
+     """
+ }
+
+S3_BUCKET=''
+
+ # Get the table name
+ if [ $# -eq 0 ]; then
+     usage;
+     exit;
+ else
+     S3_BUCKET="$1"
+ fi
+
+# delete hidden macos related file
+find . -name ".DS_Store" -delete
+# upload files
+aws s3 cp ./pipeline/ "s3://$S3_BUCKET" --recursive
